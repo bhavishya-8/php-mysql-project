a03e579252912d008f1afbeaaaaf93e8e6e08a28
Bhavishya <iambhavishyasharma@gmail.com>
2023-03-04 20:55:09 +0530

    initial commit

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 $user = 'root';
-$password = 'root';
+$password = '';
 $db = 'goals';
 $host = 'localhost';
-$port = 8889;
+$port = 3306;
 
 $link = mysqli_init();
 $success = mysqli_real_connect(
